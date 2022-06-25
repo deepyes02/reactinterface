0d700df6c0a77d8c0ee9e400f498f17aa3cd1444
deepesh_dhakal <deepesh@naphix.com.au>
2022-06-25 10:53:11 +0900

    my own toggle handle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { SiJavascript } from "react-icons/si";
+import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentList from "./data/data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
 	return (
 		<>
-			<div className="App container-fluid text-center font-thin bg-black text-white">
-				<h1 className="text-3xl mx-auto">Hello React <SiJavascript className="text-green-700 inline-block" /></h1>
-			</div>
-			<div className="search">
+			<div className="App container mx-auto mt-3 font-thin">
+				<h1 className="text-5xl">
+					<BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+				<AddAppointment />
 				<Search />
 			</div>
+			<ul>
+				{
+					AppointmentList.map(data =>
+						<AppointmentInfo 
+						key = {data.petName+data.id}
+						petName={data.petName}
+						aptDate={data.aptDate}
+						ownerName={data.ownerName}
+						aptNotes={data.aptNotes}
+						/>
+					)
+				}
+			</ul>
 		</>
 	);
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,47 @@
-import { BiCaretDown, BiSearch } from 'react-icons/bi'
+import { BiCaretDown, BiSearch, BiCheck } from 'react-icons/bi';
+
+const DropDown = () => {
+	return (
+		<div className="origin-top-right right-0 absolute mt-2 w-56
+		rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+			<div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+				<div
+					className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+					role="menuitem">Pet Name <BiCheck /></div>
+				<div
+					className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+					role="menuitem">Owner Name  <BiCheck /></div>
+				<div
+					className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+					role="menuitem">Date <BiCheck /></div>
+				<div
+					className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+					role="menuitem">Asc <BiCheck /></div>
+				<div
+					className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+					role="menuitem">Desc <BiCheck /></div>
+			</div>
+		</div>
+	);
+}
+
 const Search = () => {
 	return (
-		<div className="bg-slate-800 py-1 px-1">
-			<div className="relative shadow-sm">
+		<div className="py-0">
+			<div className="relative rounded-md shadow-sm">
 				<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-					<BiSearch />
+					<BiSearch className='text-black 3xl' />
 					<label htmlFor="query" className="sr-only" />
 				</div>
-				<input type="text" name="query" className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full text-sm" />
-				<div className='absolute inset-y-0 right-0 flex items-center'>
+				<input type="text" name="query" id="query"
+					className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
+				<div className="absolute inset-y-0 right-0 flex items-center">
 					<div>
 						<button type="button"
-							className='justify-center px-4 py-0 bg-blue-400 border-blue-400 border-2 text-sm text-white'>
+							className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
 							Sort By <BiCaretDown className="ml-2" />
 						</button>
+						<DropDown />
 					</div>
 				</div>
 			</div>
@@ -23,3 +51,4 @@ const Search = () => {
 
 
 export default Search;
+
